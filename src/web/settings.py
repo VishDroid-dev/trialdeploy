4a05df644c62dd99eba93bfdcf10a993365fe94a
VishDroid-dev <vk2345732@gmail.com>
2020-09-15 10:12:58 -0700

    done

--- src/web/settings.py
+++ src/web/settings.py
@@ -49,7 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ,]
 
 ROOT_URLCONF = 'web.urls'
